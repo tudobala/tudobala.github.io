f63627a631b4b313d11553aa06044eac00e43626
William Huang <tudobala@gmail.com>
2019-12-18 13:14:49 -0300

    add build

---
+++ build/precache-manifest.e78ee5e1be80b93fb6a9643c92dd9827.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e9722b3ad4d5d64a38b5980e480fc3bb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "85ca6aaca4be55aab475",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "600405c0bdd8d89dc4a3",
+    "url": "/static/js/2.4967635a.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.4967635a.chunk.js.LICENSE"
+  },
+  {
+    "revision": "85ca6aaca4be55aab475",
+    "url": "/static/js/main.57dfd04a.chunk.js"
+  },
+  {
+    "revision": "177d52efa2c7db582a05",
+    "url": "/static/js/runtime-main.5b50bb1e.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
