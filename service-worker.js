2c1601d442540ffa43fd732ebb37422599e81de6
William Huang <tudobala@gmail.com>
2019-12-30 13:18:21 -0300

    update

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.1f0f5e677b6c2b45160c4a3c2b0895a4.js"
+  "/precache-manifest.c8fd02cb36e5e76608d3c28b3d07f363.js"
 );
 
 self.addEventListener('message', (event) => {
